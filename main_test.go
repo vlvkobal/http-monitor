f3b42d5e98298d4517c4e9d3a14c0a0e936345be
Vladimir Kobal <vlvkoba@gmail.com>
2024-06-04 20:20:03 +0200

    Add end-to-end tests for pcap files

---
+++ main_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func TestPCAPFiles(t *testing.T) {
+	tests := []struct {
+		name         string
+		pcapFile     string
+		summary      bool
+		expectedFile string
+	}{
+		{
+			name:         "Test Diverse",
+			pcapFile:     "testdata/test-diverse.pcap",
+			summary:      false,
+			expectedFile: "testdata/test-diverse-expected.txt",
+		},
+		{
+			name:         "Test Gaps",
+			pcapFile:     "testdata/test-gaps.pcap",
+			summary:      false,
+			expectedFile: "testdata/test-gaps-expected.txt",
+		},
+	}
+
+	// Compile the program
+	exePath := "./http-monitor"
+	cmd := exec.Command("go", "build", "-o", exePath)
+	if err := cmd.Run(); err != nil {
+		t.Fatalf("failed to compile program: %s", err)
+	}
+	defer os.Remove(exePath) // Clean up the executable after tests
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Create a temporary file to capture stdout
+			tmpFile, err := ioutil.TempFile("", "stdout")
+			if err != nil {
+				t.Fatalf("failed to create temporary file: %s", err)
+			}
+			defer os.Remove(tmpFile.Name())
+
+			// Run the command
+			cmd := exec.Command(exePath, "-f", tt.pcapFile)
+			if tt.summary {
+				cmd.Args = append(cmd.Args, "-s")
+			}
+			cmd.Stdout = tmpFile
+			cmd.Stderr = &bytes.Buffer{}
+
+			err = cmd.Run()
+			if err != nil {
+				t.Fatalf("cmd.Run() failed with %s\nStderr: %s", err, cmd.Stderr.(*bytes.Buffer).String())
+			}
+
+			// Read captured output
+			tmpFile.Seek(0, 0) // Rewind to the beginning of the file
+			capturedOutput, err := ioutil.ReadAll(tmpFile)
+			if err != nil {
+				t.Fatalf("failed to read captured output: %s", err)
+			}
+
+			t.Logf("Captured Output:\n%s", capturedOutput)
+
+			// Read expected output
+			expectedOutput, err := ioutil.ReadFile(tt.expectedFile)
+			if err != nil {
+				t.Fatalf("failed to read expected output file: %s", err)
+			}
+
+			// Compare outputs
+			if string(capturedOutput) != string(expectedOutput) {
+				t.Errorf("output does not match expected\nExpected:\n%s\nGot:\n%s", string(expectedOutput), string(capturedOutput))
+			}
+		})
+	}
+}
